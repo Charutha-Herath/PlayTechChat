19180160b822789ca2ab8eafc9302fd6c4524e1c
Charutha-Herath <charuh2023@gmail.com>
2024-01-28 21:54:25 +0530

    Server done

---
+++ src/main/java/org/example/Controller/ServerFormController.java
@@ -0,0 +1,33 @@
+package org.example.Controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.example.Server.ClientHandler;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class ServerFormController {
+
+
+    public void btnClientAddOnAction(MouseEvent mouseEvent) {
+
+    }
+
+}
