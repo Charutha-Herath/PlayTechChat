1ba79566cc3b08309701f8bd9fad4d8576e9901f
Charutha-Herath <charuh2023@gmail.com>
2024-01-29 10:43:57 +0530

    Load client

--- src/main/java/org/example/Controller/LoginFormController.java
+++ src/main/java/org/example/Controller/LoginFormController.java
@@ -3,11 +3,18 @@ package org.example.Controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXPasswordField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
+import org.example.Model.LoginModel;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class LoginFormController {
     public JFXComboBox UsernameCombo;
@@ -16,6 +23,9 @@ public class LoginFormController {
     public JFXPasswordField TxtLoginPassword;
     public AnchorPane root;
 
+    public void initialize() throws SQLException {
+        loadClients();
+    }
 
     public void btnLoginOnAction(ActionEvent actionEvent) {
 
@@ -28,4 +38,16 @@ public class LoginFormController {
     public void btnSigninOnAction(ActionEvent actionEvent) {
 
     }
+
+    public void loadClients() throws SQLException {
+
+        ObservableList<String> obList = FXCollections.observableArrayList();
+        List<String> names = LoginModel.getClientName();
+
+        for (String name : names) {
+            obList.add(name);
+        }
+        UsernameCombo.setItems(obList);
+    }
+
 }
